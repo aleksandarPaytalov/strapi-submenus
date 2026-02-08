9ae8298ecf773f4f30a4a08900a939b3269fed4b
aleksandarPaytalov <apaytalov89@gmail.com>
2026-02-08 18:44:59 +0200

    submenu component implemented and its css

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,4 +1,30 @@
+import { useGlobalContext } from "./Context";
+import sublinks from "./data";
+
 const Submenu = () => {
-  return <div>Submenu</div>;
+  const { pageId, setPageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+
+  return (
+    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, icon, label } = link;
+          return (
+            <a key={id} href={url}>
+              {icon} {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
